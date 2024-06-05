00bc9355dfbb063d69a61e154bbe46ba7df2fa3a
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2024-06-05 19:55:46 +0300

    login page content

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -16,26 +16,36 @@ export default function LoginPage() {
       <form className="flex flex-col gap-6">
         <div className="flex flex-col gap-1">
           <label className="text-xs text-dark-grey">Email address</label>
-          <input
-            type="email"
-            placeholder="e.g alex@email.com"
-            className="border border-borders border-solid rounded-lg py-3 px-4"
-          />
-          <PiEnvelopeSimpleFill />
+          <div className="relative">
+            <input
+              type="email"
+              placeholder="e.g alex@email.com"
+              className="border border-borders border-solid rounded-lg py-3 px-4 ps-10 w-full text-dark-grey"
+            />
+            <PiEnvelopeSimpleFill
+              className="absolute top-1/2 left-4 transform -translate-y-1/2
+"
+            />
+          </div>
         </div>
         <div className="flex flex-col gap-1">
           <label className="text-xs text-dark-grey">Password</label>
-          <input
-            type="password"
-            placeholder="Enter your password"
-            className="border border-borders border-solid rounded-lg py-3 px-4"
-          />
-          <PiLockKeyFill />
+          <div className="relative">
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="border border-borders border-solid rounded-lg py-3 px-4 ps-10 w-full text-dark-grey"
+            />
+            <PiLockKeyFill
+              className="absolute top-1/2 left-4 transform -translate-y-1/2
+"
+            />
+          </div>
         </div>
         <button>Login</button>
       </form>
 
-      <div className="mt-6">
+      <div className="mt-6 text-center">
         <p>Don’t have an account?</p>
         <Link to="/create-account">Create account</Link>
       </div>
