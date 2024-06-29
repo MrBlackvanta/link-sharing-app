ddda55ba83f52f37f4c737558867633b662490ac
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2024-06-29 15:04:43 +0300

    adding links page html/assets

--- src/pages/LinksPage.tsx
+++ src/pages/LinksPage.tsx
@@ -1,3 +1,28 @@
+import { Button } from "components";
+
 export default function LinksPage() {
-  return <div>LinksPage</div>;
+  return (
+    <>
+      <h1>Customize your links</h1>
+      <p>
+        Add/edit/remove links below and then share all your profiles with the
+        world!
+      </p>
+      <Button>+ Add new link</Button>
+      <picture>
+        <source srcSet="links-empty.avif" type="image/avif" />
+        <img
+          src="links-empty.png"
+          alt="hand tapping a phone with 2 paper pages at the back"
+        />
+      </picture>
+      <h2>Let’s get you started</h2>
+      <p>
+        Use the “Add new link” button to get started. Once you have more than
+        one link, you can reorder and edit them. We’re here to help you share
+        your profiles with everyone!
+      </p>
+      <Button>Save</Button>
+    </>
+  );
 }
