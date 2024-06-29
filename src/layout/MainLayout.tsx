c1c365b03a3274e1c2744784cc79c84bd4f4910b
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2024-06-29 19:30:08 +0300

    cleaning main layout

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,15 +1,12 @@
 import { Header } from "components";
-import { useUser } from "hook/useUser";
 import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
-  const { user } = useUser();
-
   return (
     <>
       <Toaster />
-      {user && <Header />}
+      <Header />
       <Outlet />
     </>
   );
