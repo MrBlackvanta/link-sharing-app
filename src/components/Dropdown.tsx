de9852a02867f45b6a3de390d7cebb3aebc35faf
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2024-07-07 07:33:30 +0300

    clickcing out side would close the ddl

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useState, useEffect, useRef } from "react";
 import { IoChevronDownOutline } from "react-icons/io5";
 import { Platform as PlatformType } from "types";
 
@@ -14,6 +14,7 @@ export default function Dropdown({
   setSelectedPlatform,
 }: DropdownProps) {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   function handlePlatformsMenu(platform?: PlatformType) {
     if (!platform) {
@@ -24,11 +25,26 @@ export default function Dropdown({
     setOpenMenu(false);
   }
 
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setOpenMenu(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="relative">
+    <div className="relative" ref={dropdownRef}>
       <div
         className="flex cursor-pointer items-center justify-between gap-3 rounded-lg border border-borders bg-white px-4 py-3"
-        onClick={() => handlePlatformsMenu(undefined)}
+        onClick={() => handlePlatformsMenu()}
       >
         <selectedPlatform.icon className="text-neutral-500" />
         <span className="body-m flex-1">{selectedPlatform.label}</span>
@@ -36,14 +52,14 @@ export default function Dropdown({
       </div>
       {openMenu && (
         <div className="scrollbar-hide absolute top-[calc(100%_+12px)] z-10 max-h-52 w-full overflow-y-auto rounded-lg border-borders bg-white px-4 py-3 shadow-black-shadow">
-          {PLATFORMS.map((el, idx) => (
+          {PLATFORMS.map((platform, idx) => (
             <div
               className={`flex cursor-pointer items-center gap-3 border-b border-borders py-3 text-neutral-500 hover:text-purple ${idx === PLATFORMS.length - 1 ? "last:border-b-0" : ""}`}
               key={idx}
-              onClick={() => handlePlatformsMenu(el)}
+              onClick={() => handlePlatformsMenu(platform)}
             >
-              <el.icon className="text-inherit" />
-              <span className="body-m flex-1">{el.label}</span>
+              <platform.icon className="text-inherit" />
+              <span className="body-m flex-1">{platform.label}</span>
             </div>
           ))}
         </div>
